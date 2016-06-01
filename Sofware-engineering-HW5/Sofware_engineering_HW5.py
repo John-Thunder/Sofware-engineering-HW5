acbd1eaf4394f0ba129ac198f16a2265b17c6c90
John-Thunder <wnz9u3lb@gmail.com>
2016-06-02 01:59:33 +0800

    module_calc_discount
    模組化折扣

--- Sofware-engineering-HW5/Sofware_engineering_HW5.py
+++ Sofware-engineering-HW5/Sofware_engineering_HW5.py
@@ -15,37 +15,38 @@ def input_data():
         if re.match('[1-2]{1}',time) is None:
             print("Input Error")
             break
-        num_aduit=input(u"Enter the numner of aduit(1-999) :")
-        if re.match('[1-9]{1,3}',num_aduit) is None:
+        num_aduit=input(u"Enter the numner of aduit(0-999) :")
+        if re.match('[0-9]{1,3}',num_aduit) is None:
             print("Input Error")
             break
-        num_child=input(u"Enter the numner of child(1-999) :")
-        if re.match('[1-9]{1,3}',num_child) is None:
+        num_child=input(u"Enter the numner of child(0-999) :")
+        if re.match('[0-9]{1,3}',num_child) is None:
             print("Input Error")
             break
         return int(day),int(time),int(num_aduit),int(num_child)
 
+def calc_discount(num_aduit,num_child):
+    people_discount=int((num_aduit+num_child)/3)
+    people_count=num_aduit+num_child
+    if num_child > people_discount:
+        num_child-=people_discount
+    else:
+        people_discount-=num_child
+        num_child=0
+        num_aduit-=people_discount
+    return num_aduit,num_child,people_count
+
 while True:
     day,time,num_aduit,num_child=input_data()
     if day==1 and time==1:
-        people_discount=int((num_aduit+num_child)/3)
-        if num_child > people_discount:
-            num_child-=people_discount
-        else:
-            people_discount-=num_child
-            num_child=0
-            num_aduit-=people_discount
+        num_aduit,num_child,people_count=calc_discount(num_aduit,num_child)
         money=(week_price_aduit*num_aduit+num_child*week_price_child)
 
     if day==2 or time==2:
-        people_discount=int((num_aduit+num_child)/3)
-        if num_child > people_discount:
-            num_child-=people_discount
-        else:
-            people_discount-=num_child
-            num_child=0
-            num_aduit-=people_discount
+        num_aduit,num_child,people_count=calc_discount(num_aduit,num_child)
 
         money=(holiday_price_aduit*num_aduit+num_child*holiday_price_child)*holiday_bonus
 
-        print(int(money))
+    if (people_count)>=10:
+        money=money*0.95
+    print(round(money))
